ded24cb49cac1846e4b1776cc1e0574637dd99ba
mcanbildiren <mcanbildiren@hotmail.com>
2022-08-10 15:10:27 +0300

    code first

--- DBExample2/Models/BootcampContext.cs
+++ DBExample2/Models/BootcampContext.cs
@@ -5,13 +5,23 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace DBExample2.Models
 {
-    public partial class BootcampContext : DbContext
+    public class BootcampContext : DbContext
     {
-        public virtual DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-                optionsBuilder.UseSqlServer("Data Source=HN-PC87;Initial Catalog=Bootcamp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=HN-PC87;Initial Catalog=Bootcamp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+            // Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(p =>
+            {
+                p.Property(x => x.Price).HasPrecision(9, 2);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- DBExample2/Models/Product.cs
+++ DBExample2/Models/Product.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DBExample2.Models
 {
-    public partial class Product
+    //[Table("ProductTB",Schema ="product")]
+    public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; } = null!;
+
+        //[Required]
+        //[Column("PName")]
+        public string Name { get; set; }
+
+        //[Column(TypeName ="decimal(9,2)")]
         public decimal Price { get; set; }
         public short Stock { get; set; }
         public bool IsPublish { get; set; }
+
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+
     }
 }
